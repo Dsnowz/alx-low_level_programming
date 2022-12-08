87cd8586d1b27a2d3311500feb49956023a14ff6
olusanmioginni <olusanmioginni@gmail.com>
2022-12-08 15:00:43 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * * main - Receives no arguement value
  *  *
- *   * Return: Always 0 (success)
+ *   * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	return (0);
 }
