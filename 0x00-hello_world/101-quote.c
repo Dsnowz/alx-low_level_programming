a78639434af260e81578e107074cdc7074ea6954
olusanmioginni <olusanmioginni@gmail.com>
2022-12-11 08:30:00 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1);
+
+	return (0);
 }
