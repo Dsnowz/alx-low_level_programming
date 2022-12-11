bd071a8d5cd170522ec70fe415c5740618b0d101
olusanmioginni <olusanmioginni@gmail.com>
2022-12-11 05:29:04 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Description : A C program that prints using the printf function
+ * main - Entry
  *
  * Return: Always 0 (Success)
  */
-init main(void)
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-	
