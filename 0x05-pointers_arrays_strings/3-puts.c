b34afbacf9c54c7b194d62799816c15f42dd7f19
olusanmioginni <olusanmioginni@gmail.com>
2022-12-20 13:30:54 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * _puts - a function that prints a string to stdout
- * followed by a new line 
+ * followed by a new line
  * @str: The string to be printed.
  */
-
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 		_putchar(*str++);
 
 	_putchar('\n');
