f95c720916d1b53ab9dcb8f1044c0f842059b7d5
olusanmioginni <olusanmioginni@gmail.com>
2022-12-20 16:26:01 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,9 +18,11 @@ int _atoi(char *s)
 
 		else if (*s >= '0' && *s <= '9')
 				num = (num * 10) + (*s - '0');
-				else if (num > 0)
-				break;
-		} while (*s++);
 
-		return (num * sign);
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
