df2914e9e716166db514d5b49e8ea9a6662da50f
olusanmioginni <olusanmioginni@gmail.com>
2023-01-02 12:56:28 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ * _memset -  function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @S: the chaine
+ * @b: the char
+ * @n: the int
+ *
+ * Return: 1 or 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
